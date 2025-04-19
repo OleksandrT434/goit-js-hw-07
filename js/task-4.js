d04ce8d0e6de4eaf7f11a2f0657857b0b10180a4
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-04-19 20:17:46 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const btnText = document.querySelector('button');
 
+
+btnText.textContent = btnText.textContent.replace("Login", "Log in");
+
+
+const userForm = document.querySelector('.login-form');
+
+userForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const mail = e.target.elements.email.value.trim();
+    const pass = e.target.elements.password.value.trim();
+    
+    if (mail === '' || pass === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const formData = {
+    mail,
+    pass
+  };
+
+    console.log(formData);
+    e.target.reset();
+    
+})
